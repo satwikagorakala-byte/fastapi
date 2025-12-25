0980da76964105c59861031c728a4edd62eff899
satwikagorakala-byte <satwikagorakala@gmail.com>
2025-12-25 11:40:45 +0530

    removed initial class from the file

--- bank acc.py
+++ bank acc.py
@@ -1,40 +1,4 @@
 class BankAccount:
-    def __init__(self,AccountName,InitialDeposit,Balance=0):
-        self.AccountName=AccountName
-        self.InitialDeposit=InitialDeposit
-        self.Balance=InitialDeposit
-    
-    def deposite(self,DepositAmount):
-        self.DepositAmount=DepositAmount
-        if(self.DepositAmount>=100):
-            ccccself.Balance=self.Balance+self.DepositAmount
-            return self.Balance
-        else:
-            return "not allowed"
-        
-    def withdraw(self,amount):
-        self.amount=amount
-        if(amount>=100):
-            self.Balance=self.Balance-self.amount
-            return self.Balance
-        
-
-
-p=BankAccount("Satwika",1000)
-p.deposite(1000)
-p.withdraw(500)
-
-p.deposite(1500)
-p.withdraw(300)
-
-p.deposite(900)
-p.withdraw(300)
-
-
-
-
-
-class BankAccount:
     def __init__(self,AccountName,InitialDeposit):
         self.Balance = 0
         self.AccountName=AccountName
